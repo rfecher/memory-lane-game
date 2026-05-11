1732f93b267ebd4a61af24757a8ed6f2571e5002
Rich Fecher <richard.fecher@vantor.com>
2026-05-11 17:06:48 -0400

    updates

--- src/hooks/GameContext.jsx
+++ src/hooks/GameContext.jsx
@@ -16,7 +16,12 @@ const pickWeightedCategory = () => {
 }
 
 function shuffleArray(arr) {
-  return [...arr].sort(() => Math.random() - 0.5)
+  const a = [...arr]
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    ;[a[i], a[j]] = [a[j], a[i]]
+  }
+  return a
 }
 
 function shuffleAnswers(question) {
